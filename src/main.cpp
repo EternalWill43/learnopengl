d7950b0e63867408ce25a246cd3f5398a6acb426
EternalWill43 <haler43@icloud.com>
2024-06-06 10:48:47 -0400

    feat: Hello Triangle, exercise 1

--- src/main.cpp
+++ src/main.cpp
@@ -86,18 +86,20 @@ int main()
   InitializeOpenGlFunctions();
   ValidateGLFunctions();
 
-  Vec3F2 vertices[] = {{0.5f, 0.5f}, {0.5f, -0.5f}, {-0.5f, -0.5f}, {-0.5f, 0.5f}};
-  unsigned int indices[] = {0, 1, 3, 1, 2, 3};
+  Vec3F2 vertices[] = {{-0.5f, 0.0f}, {0.5f, 0.0f}, {0.0f, 0.5f},
+                       {-0.5f, 0.0f}, {0.5f, 0.0f}, {0.0f, -0.5f}};
+  // Vec3F2 vertices[] = {{0.5f, 0.5f}, {0.5f, -0.5f}, {-0.5f, -0.5f}, {-0.5f, 0.5f}};
+  // unsigned int indices[] = {0, 1, 3, 1, 2, 3};
 
-  unsigned int VAO, VBO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  unsigned int VAO[2], VBO[2], EBO;
+  glGenVertexArrays(1, &VAO[0]);
+  glBindVertexArray(VAO[0]);
+  glGenBuffers(1, &VBO[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   glGenBuffers(1, &EBO);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   /* VERTEX SHADER */
   GLuint vertexShader;
@@ -131,8 +133,8 @@ int main()
     processInput(window);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    // glDrawArrays(GL_TRIANGLES, 0, 6);
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
     glfwPollEvents();
     glfwSwapBuffers(window);
     // RotateTriangles(vertices, sizeof(vertices));
